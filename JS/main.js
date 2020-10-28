481a0c19a113e4e2d47f7c0d6d5b1eb42db4bbf4
Pempium4 <front.shipper@gmail.com>
2020-10-28 13:47:51 +0200

    'GULP.Concat

---
+++ JS/main.js
@@ -0,0 +1,2 @@
+console.log(calculate(4, 6));
+console.log(calculate(3, 11));
---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+const gulp = require('gulp');
+const gulpConcat = require('gulp-concat');
+const gulpAutoPrefix = require('gulp-autoprefixer');
+const gulpCleanCSS = require('gulp-clean-css');
+const gulpUglify = require('gulp-uglify');
+const gulpTerser = require('gulp-terser');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+const styleFiles = [
+    './CSS/main.css',
+    './CSS/media.css'
+];
+const scriptFiles = [
+    './JS/lib.js',
+    './JS/main.js'
+]
+
+function styles(){
+    return gulp.src(styleFiles)
+        .pipe(gulpConcat('style.css'))
+        .pipe(gulpAutoPrefix({
+            browsers: ['last 2 version'],
+            cascade: false
+        }))
+        .pipe(gulpCleanCSS({
+            level: 2
+        }))
+        .pipe(gulp.dest('./Build'))
+        .pipe(browserSync.stream());
+}
+function scripts(){
+    return gulp.src(scriptFiles)
+        .pipe(gulpConcat('main.js'))
+        .pipe(gulpUglify({
+            toplevel: true
+        }))
+        //or .pipe(gulpTerser()) //supports ES6
+        .pipe(gulp.dest('./Build'))
+        .pipe(browserSync.stream());
+}
+function clean(){
+    return del(['build/*']);
+}
+function watch(){
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./CSS/**/*.css', styles); //** - любая директория
+    gulp.watch('./JS/**/*.js', scripts);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+}
+
+gulp.task('styles', styles);
+gulp.task('scripts', scripts);
+gulp.task('del', clean);
+gulp.task('watch', watch);
+
+gulp.task('build', gulp.series(clean, gulp.parallel(styles, scripts)));
+gulp.task('dev', gulp.series('build', 'watch'));
