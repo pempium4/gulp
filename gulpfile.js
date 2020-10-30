0a668638d676bcb267aaa4f8c907e03b6b5151d8
Pempium4 <front.shipper@gmail.com>
2020-10-30 16:27:02 +0200

    Ready pack

--- gulpfile.js
+++ gulpfile.js
@@ -3,56 +3,67 @@ const gulpConcat = require('gulp-concat');
 const gulpAutoPrefix = require('gulp-autoprefixer');
 const gulpCleanCSS = require('gulp-clean-css');
 const gulpUglify = require('gulp-uglify');
-//const gulpTerser = require('gulp-terser');
+// const gulpTerser = require('gulp-terser');
 const del = require('del');
 const browserSync = require('browser-sync').create();
 
 const gulpSass = require('gulp-sass');
-//const gulpSourceMaps = require('gulp-sourcemaps');
+const gulpSourceMaps = require('gulp-sourcemaps');
 
-function stylesSCSS(){
+// const imagemin = require('gulp-imagemin');
+
+const gulpRename = require('gulp-rename');
+
+
+gulp.task('del', () => {
+    return del(['Build/*']);
+});
+gulp.task('styles', () =>{
     return gulp.src('./SCSS/**/*.scss')
-        .pipe(gulpConcat('style.scss'))
-        // .pipe(gulpSourceMaps.init())
+        .pipe(gulpSourceMaps.init()) //для отображения SCSS строк
         .pipe(gulpSass().on('Error', gulpSass.logError))
-        // .pipe(gulpSourceMaps.write('./'))
         .pipe(gulpAutoPrefix({
-            browsers: ['last 2 version'],
             cascade: false
         }))
-        .pipe(gulpCleanCSS({
-            level: 2
+        .pipe(gulpConcat('style.css'))
+        .pipe(gulpCleanCSS())
+        .pipe(gulpRename({
+            suffix: '.min'
         }))
+        .pipe(gulpSourceMaps.write('./'))
         .pipe(gulp.dest('./Build/'))
         .pipe(browserSync.stream());
-}
-function scripts(){
+});
+gulp.task('scripts', () => {
     return gulp.src('./JS/**/*.js')
         .pipe(gulpConcat('main.js'))
         .pipe(gulpUglify({
             toplevel: true
         }))
         //or .pipe(gulpTerser()) //supports ES6
+        .pipe(gulpRename({
+            suffix: '.min'
+        }))
         .pipe(gulp.dest('./Build'))
         .pipe(browserSync.stream());
-}
-function clean(){
-    return del(['Build/*']);
-}
-function watch(){
+});
+/*gulp.task('img-compress', () =>{
+    return gulp.src('./img/!**')
+        .pipe(imagemin({
+            progressive: true
+        }))
+        .pipe(gulp.dest('./Build/img'))
+})*/
+gulp.task('watch', () => {
     browserSync.init({
         server: {
             baseDir: './'
         }
     });
-    gulp.watch('./SCSS/**/*.scss', stylesSCSS); //** - любая директория
-    gulp.watch('./JS/**/*.js', scripts);
+    // gulp.watch('./img/**', gulp.series('img-compress'));
+    gulp.watch('./SCSS/**/*.scss', gulp.series('styles')); //** - любая директория
+    gulp.watch('./JS/**/*.js', gulp.series('scripts'));
     gulp.watch('./*.html').on('change', browserSync.reload);
-}
+});
 
-gulp.task('styles', stylesSCSS);
-gulp.task('scripts', scripts);
-gulp.task('del', clean);
-gulp.task('watch', watch);
-
-//gulp.task('build', gulp.series(del, watch)));
+gulp.task('default', gulp.series('del', gulp.parallel('styles', 'scripts'), 'watch'));
